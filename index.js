4b6683ee25f0995a8de807707be8e58593b61016
Brian Woodward <brian.woodward@gmail.com>
2015-10-06 19:47:17 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * scaffolds <https://github.com/doowb/scaffolds>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
