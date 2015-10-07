736cf2466c2c79d5bd3085b8683732e09c735c99
Brian Woodward <brian.woodward@gmail.com>
2015-10-07 00:55:27 -0400

    updating to handle downloading target files and storing them in a global location.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var mocha = require('gulp-mocha');
 var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
+var writeFile = require('write');
+
+var utils = require('./lib/utils');
+var Scaffolds = require('./');
 
 var lint = ['index.js', 'lib/**/*.js'];
 
@@ -28,6 +32,38 @@ gulp.task('lint', function () {
   return gulp.src(lint.concat('test/*.js'))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('init', function (done) {
+  var scaffolds = new Scaffolds();
+  scaffolds.init();
+  scaffolds.metadata.addDependencies({
+    assemblefile: 'doowb/scaffold-assemblefile',
+    docs: 'doowb/scaffold-docs',
+    gulpfile: 'doowb/scaffold-gulpfile',
+    tests: 'doowb/scaffold-tests',
+    verbmd: 'doowb/scaffold-verbmd',
+  });
+  writeFile('manifest.json', JSON.stringify(scaffolds.metadata, null, 2), done);
+});
+
+gulp.task('install', function (done) {
+  var scaffolds = new Scaffolds();
+
+  var metadata = utils.metadata();
+  metadata.load(require('./manifest.json'));
+
+  scaffolds.init(metadata.toJSON());
+  scaffolds.install(function (err, metadata) {
+    if (err) return done(err);
+    done();
+  });
+});
+
+gulp.task('clear:cache', function (done) {
+  var scaffolds = new Scaffolds();
+  scaffolds.store.del({force: true});
+  process.nextTick(done);
 });
 
 gulp.task('default', ['lint', 'test']);
